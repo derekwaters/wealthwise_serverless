e74c52728876b29df05b645776b5fb170cb77e04
Derek <derek@frisbeeworld.com>
2025-01-11 09:37:32 +1100

    More testing changes

--- func-cost-suggest/index.js
+++ func-cost-suggest/index.js
@@ -1,19 +1,22 @@
+const { CloudEvent } = require('cloudevents');
+
 /**
- * Your HTTP handling function, invoked with each request. This is an example
- * function that echoes its input to the caller, and returns an error if
- * the incoming request is something other than an HTTP POST or GET.
+ * Your CloudEvent handling function, invoked with each request.
+ * This example function logs its input, and responds with a CloudEvent
+ * which echoes the incoming event data
  *
- * In can be invoked with 'func invoke'
+ * It can be invoked with 'func invoke'
  * It can be tested with 'npm test'
  *
  * @param {Context} context a context object.
  * @param {object} context.body the request body if any
- * @param {object} context.query the query string deserialized as an object, if any
+ * @param {object} context.query the query string deserialzed as an object, if any
  * @param {object} context.log logging object with methods for 'info', 'warn', 'error', etc.
  * @param {object} context.headers the HTTP request headers
  * @param {string} context.method the HTTP request method
  * @param {string} context.httpVersion the HTTP protocol version
  * See: https://github.com/knative/func/blob/main/docs/function-developers/nodejs.md#the-context-object
+ * @param {CloudEvent} event the CloudEvent
  */
 
 const kafka_clientId = 'wealthwise-transact'
@@ -37,6 +40,9 @@ const handle = async (context, event) => {
   // event.data. will contain the Kafka data
   context.log.info("query", context.query);
   context.log.info("event", event);
+  context.log.info(event);
+  context.log.info(event.data);
+  context.log.info(typeof event.data);
 
   var recommendation = null;
   if (event.data.type == 'expense') {
